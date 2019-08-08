7b8ab26d149204bb0dc096200cb0e9e0d304fa8f
junglehunter96 <junglehunter96@Gmail.com>
2019-08-08 18:32:34 +0800

    fix bugs slide

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -1,104 +1,108 @@
 <template>
-  <transition class="fade">
-    <div
-      class="slide-content-wrapper"
-      v-show="menuVisible && settingVisible === 3 "
-    >
-      <transition class="slide-right">
-        <div
-          class="content"
-          v-show="settingVisible === 3"
-        >
-          <div class="content-page-wrapper">
+  <transition name="fade">
+    <div class="slide-content-wrapper" v-show="menuVisible && settingVisible === 3">
+      <transition name="slide-right">
+        <div class="content" v-if="settingVisible === 3">
+          <div class="content-page-wrapper" v-if="bookAvailable">
             <div class="content-page">
-              <component :is="settingTabItem ===1 ? content : bookmark "></component>
+              <component :is="currentTab === 1 ? content : bookmark"></component>
             </div>
             <div class="content-page-tab">
-              <div
-                class="content-page-tab-item"
-                :class="{'selected' : settingTabItem === 1}"
-                @click="changeSettingTabItem(1)"
-              >{{$t('book.navigation')}}</div>
-              <div
-                class="content-page-tab-item"
-                :class="{'selected' : settingTabItem === 2}"
-                @click="changeSettingTabItem(2)"
-              >{{$t('book.bookmark')}}</div>
+              <div class="content-page-tab-item"
+                   :class="{'selected': currentTab === 1}"
+                   @click="selectTab(1)">
+                {{$t('book.navigation')}}
+              </div>
+              <div class="content-page-tab-item"
+                   :class="{'selected': currentTab === 2}"
+                   @click="selectTab(2)">
+                {{$t('book.bookmark')}}
+              </div>
             </div>
+          </div>
+          <div class="content-empty" v-else>
+            <ebook-loading></ebook-loading>
           </div>
         </div>
       </transition>
-      <div
-        class="content-bg"
-        @click="quitMenu"
-      ></div>
+      <div class="content-bg" @click="hideTitleAndMenu()"></div>
     </div>
   </transition>
 </template>
 
 <script>
-import { ebookMixins } from '../../utils/mixins';
-import BookContent from './EbookSlideContent';
-import BookMark from './EbookSlideBookMark';
-export default {
-  mixins: [ebookMixins],
-  data () {
-    return {
-      settingTabItem: 1,
-      content: BookContent,
-      bookmark: BookMark
-    }
-  },
-  methods: {
-    changeSettingTabItem (value) {
-      this.settingTabItem = value;
+  import { ebookMixin } from '../../utils/mixin'
+  import EbookSlideContents from './EbookSlideContents'
+  import EbookSlideBookmark from './EbookSlideBookmark'
+  import EbookLoading from './EbookLoading'
+
+  export default {
+    mixins: [ebookMixin],
+    components: {
+      EbookLoading
     },
-    quitMenu (e) {
-      this.toggleTitleAndMenu()
+    data() {
+      return {
+        currentTab: 1,
+        content: EbookSlideContents,
+        bookmark: EbookSlideBookmark
+      }
+    },
+    methods: {
+      selectTab(tab) {
+        this.currentTab = tab
+      }
     }
-  },
-}
+  }
 </script>
 
-<style lang="scss" scoped>
-@import "../../assets/styles/mixin.scss";
-.slide-content-wrapper {
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: 300;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  .content {
-    flex: 0 0 85%;
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../../assets/styles/global";
+
+  .slide-content-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 300;
+    display: flex;
+    width: 100%;
     height: 100%;
-    .content-page-wrapper {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
+    .content {
+      flex: 0 0 85%;
+      width: 85%;
       height: 100%;
-      .content-page {
-        flex: 1;
-        width: 100%;
-        overflow: hidden;
-      }
-      .content-page-tab {
-        flex: 0 0 px2rem(48);
-        width: 100%;
-        height: px2rem(48);
+      .content-page-wrapper {
         display: flex;
-        .content-page-tab-item {
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        .content-page {
           flex: 1;
-          @include center;
-          font-size: px2rem(12);
+          width: 100%;
+          overflow: hidden;
+        }
+        .content-page-tab {
+          display: flex;
+          flex: 0 0 px2rem(48);
+          width: 100%;
+          height: px2rem(48);
+          .content-page-tab-item {
+            flex: 1;
+            font-size: px2rem(12);
+            @include center;
+          }
         }
       }
+      .content-empty {
+        width: 100%;
+        height: 100%;
+        @include center;
+      }
+    }
+    .content-bg {
+      flex: 0 0 15%;
+      width: 15%;
+      height: 100%;
     }
   }
-  .content-bg {
-    flex: 0 0 15%;
-    height: 100%;
-  }
-}
 </style>
